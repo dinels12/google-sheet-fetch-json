2ecffb946807b6cfb5a952e18e92c179d1f3b0af
“dinels” <dinels12@gmail.com>
2021-11-07 08:47:52 -0400

    fix prettier

--- test/Index.test.ts
+++ test/Index.test.ts
@@ -1,12 +1,12 @@
 import getData from '../src';
 
 /**
- * 
+ *
  * Remember to set the local env with:
  * for Linux:
  * export SHEET_URL=HERE THE URL
  * Check how to set the env in others SO
- * 
+ *
  */
 
 const sheet_url = process.env.SHEET_URL as string;
